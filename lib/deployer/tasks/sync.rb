de1675c4f0d83fe9c451ba26509c339389b06621
Michael van Rooijen <meskyanichi@gmail.com>
2010-07-12 11:52:49 +0200

    Expanded the Sync task. Now it is not limited to only the shared/config path. Instead, it's mirrored just like the other tasks.

--- lib/deployer/tasks/sync.rb
+++ lib/deployer/tasks/sync.rb
@@ -3,7 +3,7 @@ namespace :deploy do
     desc "Syncs the configured files from the local machine to the remote machine"
     task :files do
       files = []
-
+      
       unless respond_to?(:skip_database) and skip_database
         files << "config/database.yml"
       end
@@ -14,8 +14,16 @@ namespace :deploy do
 
       files.each do |file|
         if File.exist?(file)
-          log "Syncing local file \"#{file}\" to the shared folder (#{appname}/shared/#{file})"
-          system "rsync -vr --exclude='.DS_Store' #{file} #{user}@#{application}:#{shared_path}/config/"
+          path_parts = file.split('/')
+          file = path_parts.pop
+          if path_parts.empty?
+            path = ''
+          else
+            path = path_parts.join('/') + '/'
+          end
+          
+          log "Syncing local file \"#{path + file}\" to the shared folder (#{appname}/shared/#{path + file})"
+          system "rsync -vr --exclude='.DS_Store' #{path + file} #{user}@#{application}:#{shared_path}/#{path}"
         end
       end
     end

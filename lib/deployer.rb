ef9a6fd679676dd9f4b8a246c3a108c45ea2052d
Jacques Crocker <railsjedi@gmail.com>
2010-07-11 21:39:19 -0700

    deploy.rb is a more fitting name

--- lib/deployer.rb
+++ lib/deployer.rb
@@ -1,5 +1,5 @@
 ##
 # Returns a path that can be loaded by the "load" method inside the Capfile
 def deployer
-  File.join(File.dirname(__FILE__), 'deployer', 'setup.rb')
+  File.join(File.dirname(__FILE__), 'deployer', 'deploy.rb')
 end
